52a07bde539ddbfb31835e343517401ba37b336e
Anthony McClay <anthony.mcclay@gmail.com>
2020-09-09 23:25:42 -0400

    Added comments

--- greeting.go
+++ greeting.go
@@ -1,11 +1,16 @@
+// Package Hello - adds some simple greetings
 package greeting
 
 import "fmt"
 
+// Hello - replays with a smart Hello text
+// written by: mcclayac
 func Hello() {
 	fmt.Println("vim-go, Hello - Theze nutz")
 }
 
+// Hi - replays with a smart "Hi, Mom" text
+// written by: mcclayac
 func Hi() {
 	fmt.Println("Hi, Mom")
 }
