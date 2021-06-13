156c2f1f591bbd2551908f4fabe5b6852b433ae1
Tony <anthony.mcclay@me.com>
2021-06-13 13:22:23 -0500

    Added two new directories and two new files

---
+++ dansk/dansk.go
@@ -0,0 +1,11 @@
+package dansk
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hej - Dansk Local")
+}
+
+func Hi() {
+	fmt.Println("Hej - Dansk local")
+}
---
+++ deutsch/deutch.go
@@ -0,0 +1,11 @@
+package deutsch
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hallo - Dansk Local")
+}
+
+func Hi() {
+	fmt.Println("Hallo - Dansk local")
+}
